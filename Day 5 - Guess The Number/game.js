f50df9abdd4f821bbc091d37a63d0e55d1a57e9d
fczbkk <riki@fczbkk.com>
2022-05-04 19:45:08 +0200

    basic game logic

--- Day 5 - Guess The Number/game.js
+++ Day 5 - Guess The Number/game.js
@@ -1,21 +1,30 @@
 // Funkce pro vygenerování náhodného celého čísla mezi hodnotami minimum
 // (včetně) a maximum (včetně).
 function generateRandomNumber (minimum, maximum) {
-  let result = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
-  return result
+  let result = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+  return result;
 }
 
 window.addEventListener('load', function () {
-  let randomNumber = generateRandomNumber(0, 100)
-  console.log('random number', randomNumber)
+  let randomNumber = generateRandomNumber(0, 100);
+  console.log('random number', randomNumber);
 
-  let guessButton = document.querySelector('.guess-button')
+  let guessForm = document.querySelector('.guess-form');
 
-  let inputNumber = document.querySelector('.input-number')
+  let inputNumber = document.querySelector('.input-number');
 
-  guessButton.addEventListener('click', function () {
-    let userGuess = inputNumber.value
-    console.log('uzivatel hadal cislo', userGuess)
-  })
+  guessForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+    let userGuess = Number(inputNumber.value);
 
-})
+    if (userGuess === randomNumber) {
+      console.log('uhodla jsi')
+    } else if (userGuess > randomNumber) {
+      console.log('myslim si mensi cislo')
+    } else {
+      console.log('myslim si vetsi cislo')
+    }
+
+  });
+
+});
