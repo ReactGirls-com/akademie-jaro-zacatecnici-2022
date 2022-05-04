05d24eb5dce1bb7eab966fda1f9b35e974d86e20
fczbkk <riki@fczbkk.com>
2022-05-04 18:59:42 +0200

    reacting to user guesses

--- Day 5 - Guess The Number/game.js
+++ Day 5 - Guess The Number/game.js
@@ -1,1 +1,21 @@
-console.log('nazdar, fungujem')
+// Funkce pro vygenerování náhodného celého čísla mezi hodnotami minimum
+// (včetně) a maximum (včetně).
+function generateRandomNumber (minimum, maximum) {
+  let result = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+  return result
+}
+
+window.addEventListener('load', function () {
+  let randomNumber = generateRandomNumber(0, 100)
+  console.log('random number', randomNumber)
+
+  let guessButton = document.querySelector('.guess-button')
+
+  let inputNumber = document.querySelector('.input-number')
+
+  guessButton.addEventListener('click', function () {
+    let userGuess = inputNumber.value
+    console.log('uzivatel hadal cislo', userGuess)
+  })
+
+})
